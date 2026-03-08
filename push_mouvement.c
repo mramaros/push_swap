225d6bc8b6325561f28c1f55e0125b8c9f9a0957
Manda Heriniaina Ramarosandratana <mramaros@c1r11s11.42antananarivo.mg>
2026-03-08 15:26:40 +0300

    prsing strategy edit

--- push_mouvement.c
+++ push_mouvement.c
@@ -6,13 +6,11 @@
 /*   By: mramaros <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 15:02:09 by mramaros          #+#    #+#             */
-/*   Updated: 2026/03/08 15:18:51 by mramaros         ###   ########.fr       */
+/*   Updated: 2026/03/08 15:24:42 by mramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "push_swap.h"
-#include <stdio.h>
 
 void	push_a(t_list **stack_a, t_list **stack_b)
 {
@@ -32,56 +30,8 @@ void	push_b(t_list **stack_a, t_list **stack_b)
 
 	if (!*stack_a)
 		return ;
-	tmp = *stack_b;
-	*stack_b = (*stack_b)->next;
+	tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
 	tmp->next = *stack_b;
 	*stack_b = tmp;
-}
-
-void	print_stack(char *name, t_list *stack)
-{
-	printf("%s: ", name);
-	while (stack)
-	{
-		printf("%d ", *(int *)stack->content);
-		stack = stack->next;
-	}
-	printf("\n");
-}
-
-int	main(void)
-{
-	t_list	*stack_a = NULL;
-	t_list	*stack_b = NULL;
-
-	int	a = 1;
-	int	b = 2;
-	int	c = 3;
-	int	d = 4;
-
-	ft_lstadd_back(&stack_a, ft_lstnew(&a));
-	ft_lstadd_back(&stack_a, ft_lstnew(&b));
-	ft_lstadd_back(&stack_a, ft_lstnew(&c));
-	ft_lstadd_back(&stack_a, ft_lstnew(&d));
-
-	printf("Initial stacks\n");
-	print_stack("A", stack_a);
-	print_stack("B", stack_b);
-
-	printf("\n--- pb ---\n");
-	push_b(&stack_a, &stack_b);
-	print_stack("A", stack_a);
-	print_stack("B", stack_b);
-
-	printf("\n--- pb ---\n");
-	push_b(&stack_a, &stack_b);
-	print_stack("A", stack_a);
-	print_stack("B", stack_b);
-
-	printf("\n--- pa ---\n");
-	push_a(&stack_a, &stack_b);
-	print_stack("A", stack_a);
-	print_stack("B", stack_b);
-
-	return (0);
 }
