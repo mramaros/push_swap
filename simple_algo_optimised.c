d85d22a6a9100e18b6accf565c6b922784ccd09a
Manda Heriniaina Ramarosandratana <mramaros@c1r11s11.42antananarivo.mg>
2026-03-21 18:43:18 +0300

    final_push_swap

--- simple_algo_optimised.c
+++ simple_algo_optimised.c
@@ -6,7 +6,7 @@
 /*   By: mramaros <mramaros@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 13:21:56 by mramaros          #+#    #+#             */
-/*   Updated: 2026/03/21 09:17:32 by mramaros         ###   ########.fr       */
+/*   Updated: 2026/03/21 18:42:22 by mramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,9 @@ static void	sort_five(t_list **stack_a, t_list **stack_b, int *tab)
 
 void	simple_algo_optimised(t_list **stack_a, t_list **stack_b, int *tab)
 {
-	if (ft_lstsize(*stack_a) == 3)
+	if (ft_lstsize(*stack_a) == 2)
+		simple(stack_a, stack_b, tab);
+	else if (ft_lstsize(*stack_a) == 3)
 		sort_three(stack_a, tab);
 	else if (ft_lstsize(*stack_a) == 4)
 		simple(stack_a, stack_b, tab);
